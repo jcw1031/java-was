710705491a953a2d58fe1567822e5ac52aef87f3
jcw1031 <jcw001031@gmail.com>
2024-07-04 21:59:34 +0900

    refactor: QueryParameters 파싱 시 URL 디코딩

--- src/main/java/codesquad/http/parser/QueryParametersParser.java
+++ src/main/java/codesquad/http/parser/QueryParametersParser.java
@@ -2,10 +2,16 @@ package codesquad.http.parser;
 
 import codesquad.format.Parser;
 import codesquad.http.QueryParameters;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.Arrays;
 
 public class QueryParametersParser implements Parser<QueryParameters> {
+
+    private final Logger log = LoggerFactory.getLogger(QueryParametersParser.class);
 
     @Override
     public QueryParameters parse(String uriText) {
@@ -25,14 +31,19 @@ public class QueryParametersParser implements Parser<QueryParameters> {
         return queryParameters;
     }
 
-    private static void putValue(String pair, QueryParameters queryParameters) {
-        String[] keyValue = pair.split("=", 2);
-        if (keyValue.length == 2) {
-            String key = keyValue[0];
-            String value = keyValue[1];
-            queryParameters.addValue(key, value);
-        } else {
-            queryParameters.addValue(keyValue[0], "");
+    private void putValue(String pair, QueryParameters queryParameters) {
+        try {
+            String[] keyValue = pair.split("=", 2);
+            if (keyValue.length == 2) {
+                String key = keyValue[0];
+                String value = URLDecoder.decode(keyValue[1], "UTF-8");
+                queryParameters.addValue(key, value);
+            } else {
+                queryParameters.addValue(keyValue[0], "");
+            }
+        } catch (UnsupportedEncodingException e) {
+            log.error(e.getMessage(), e);
+            // TODO 400 Bad Request 처리하기
         }
     }
 
