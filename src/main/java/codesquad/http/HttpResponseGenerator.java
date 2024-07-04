f483e7149747ab428f86d250657a2391d00af676
jcw1031 <jcw001031@gmail.com>
2024-07-04 21:17:49 +0900

    refactor: HttpResponse를 생성하는 로직 구현
    - HttpResponseGenerator가 HttpResponse를 생성

--- src/main/java/codesquad/http/HttpResponseGenerator.java
+++ src/main/java/codesquad/http/HttpResponseGenerator.java
@@ -2,28 +2,18 @@ package codesquad.http;
 
 public class HttpResponseGenerator {
 
-    public static final String CRLF = "\r\n";
-
-    public HttpResponse sendOK(String body, String contentType) {
-        return generate(StatusCode.OK, body, contentType);
+    public HttpResponse sendOK(byte[] body, MediaType mediaType, HttpRequest httpRequest) {
+        return generate(StatusCode.OK, body, mediaType, httpRequest);
     }
 
-    public HttpResponse sendNotFound(String body, String contentType) {
-        return generate(StatusCode.NOT_FOUND, body, contentType);
+    public HttpResponse sendNotFound(HttpRequest httpRequest) {
+        return generate(StatusCode.NOT_FOUND, "<h1>404 Not Found</h1>".getBytes(), MediaType.TEXT_HTML, httpRequest);
     }
 
-    private HttpResponse generate(StatusCode statusCode, String body, String contentType) {
+    private HttpResponse generate(StatusCode statusCode, byte[] body, MediaType mediaType, HttpRequest httpRequest) {
         HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.addValue("Content-Type", contentType);
-        return new HttpResponse(statusCode, httpHeaders, body);
-    }
-
-    public byte[] generate(String body, String contentType) {
-        String response = "HTTP/1.1 200 OK" + CRLF +
-                "Content-Type: " + contentType + CRLF +
-                CRLF +
-                "Content-Length: " + body.getBytes().length;
-        return response.getBytes();
+        httpHeaders.addValue("Content-Type", mediaType.getValue());
+        return new HttpResponse(statusCode, httpRequest.httpVersion(), httpHeaders, body);
     }
 
 }
