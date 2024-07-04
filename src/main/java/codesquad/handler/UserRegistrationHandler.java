649fd103ec6b3c9024e73997a9d9e0bae71f9fe7
jcw1031 <jcw001031@gmail.com>
2024-07-04 21:10:02 +0900

    refactor: HTTP request에 대한 처리 객체 분리
    - 핸들러 추상화 RequestHandler
    - 정적 리소스를 처리하는 StaticResourceHandler
    - 회원가입 요청을 처리하는 UserRegistrationHandler
    - 핸들러 매핑을 위한 HandlerMapper

---
+++ src/main/java/codesquad/handler/UserRegistrationHandler.java
@@ -0,0 +1,13 @@
+package codesquad.handler;
+
+import codesquad.http.HttpRequest;
+import codesquad.http.HttpResponse;
+
+public class UserRegistrationHandler extends RequestHandler {
+
+    @Override
+    public HttpResponse handle(HttpRequest httpRequest) {
+        return null;
+    }
+
+}
