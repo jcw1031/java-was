40088eae916c90b76d468380e56a37944589195f
jcw1031 <jcw001031@gmail.com>
2024-07-15 23:18:13 +0900

    feat: 글쓰기 폼 조회 핸들러 구현
    - GET /article 요청 처리
    - AuthenticatedHandler를 추가해 인증을 선행하도록 구현
    - HandlersMapper에 /article로 추가

---
+++ src/main/java/codesquad/handler/ArticleHandler.java
@@ -0,0 +1,34 @@
+package codesquad.handler;
+
+import codesquad.error.HttpStatusException;
+import codesquad.http.HttpRequest;
+import codesquad.http.HttpResponse;
+import codesquad.http.MediaType;
+import codesquad.http.StatusCode;
+import codesquad.resource.DirectoryIndexResolver;
+import codesquad.resource.Resource;
+
+public class ArticleHandler extends AuthenticatedHandler {
+
+    private static ArticleHandler instance;
+
+    private final DirectoryIndexResolver directoryIndexResolver = DirectoryIndexResolver.getInstance();
+
+    private ArticleHandler() {
+    }
+
+    public static ArticleHandler getInstance() {
+        if (instance == null) {
+            instance = new ArticleHandler();
+        }
+        return instance;
+    }
+
+    @Override
+    protected HttpResponse handleGet(HttpRequest httpRequest) {
+        String uri = httpRequest.uri();
+        Resource resource = directoryIndexResolver.resolve(uri)
+                .orElseThrow(() -> new HttpStatusException(StatusCode.NOT_FOUND, "[ERROR] 파일을 찾을 수 없습니다."));
+        return responseGenerator.sendOK(resource.getContent(), MediaType.find(resource.getExtension()), httpRequest);
+    }
+}
