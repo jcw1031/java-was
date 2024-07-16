e325c8f6e7024860db8d236ab7b2be775d53dcae
jcw1031 <jcw001031@gmail.com>
2024-07-16 21:45:06 +0900

    feat: 아티클 작성 요청 처리
    - ArticleHandler에서 handlePost() 재정의
    - 본문이 비어있지 않은지 검증
    - 시작과 끝 공백 제거 후 저장
    - 작성자 ID와 함께 저장

--- src/main/java/codesquad/handler/ArticleHandler.java
+++ src/main/java/codesquad/handler/ArticleHandler.java
@@ -1,12 +1,16 @@
 package codesquad.handler;
 
+import codesquad.database.ArticleRepository;
 import codesquad.error.HttpStatusException;
+import codesquad.handler.dto.WriteArticleRequest;
 import codesquad.http.HttpRequest;
 import codesquad.http.HttpResponse;
 import codesquad.http.MediaType;
 import codesquad.http.StatusCode;
 import codesquad.http.session.SessionContext;
 import codesquad.http.session.SessionContextHolder;
+import codesquad.model.Article;
+import codesquad.model.User;
 import codesquad.resource.DirectoryIndexResolver;
 import codesquad.resource.Resource;
 import codesquad.resource.transform.HtmlTransformer;
@@ -16,6 +20,8 @@ public class ArticleHandler extends AuthenticatedHandler {
     private static ArticleHandler instance;
 
     private final DirectoryIndexResolver directoryIndexResolver = DirectoryIndexResolver.getInstance();
+    private final ObjectMapper objectMapper = ObjectMapper.getInstance();
+    private final ArticleRepository articleRepository = ArticleRepository.getInstance();
 
     private ArticleHandler() {
     }
@@ -36,5 +42,24 @@ public class ArticleHandler extends AuthenticatedHandler {
         SessionContext context = SessionContextHolder.getContext();
         String replacedHtml = HtmlTransformer.replaceUserHeader(content, context.user());
         return responseGenerator.sendOK(replacedHtml.getBytes(), MediaType.find(resource.getExtension()), httpRequest);
+    }
+
+    @Override
+    protected HttpResponse handlePost(HttpRequest httpRequest) {
+        String body = httpRequest.body()
+                .orElseThrow(() -> new HttpStatusException(StatusCode.BAD_REQUEST, "[ERROR] request body가 없습니다."));
+        WriteArticleRequest writeArticleRequest = objectMapper.readQueryString(body, WriteArticleRequest.class);
+
+        String content = writeArticleRequest.content()
+                .trim();
+        if (content.isBlank()) {
+            throw new HttpStatusException(StatusCode.BAD_REQUEST, "[ERROR] 글 내용이 비어있습니다.");
+        }
+
+        User user = SessionContextHolder.getContext()
+                .user();
+        Article article = new Article(content, user.getId());
+        articleRepository.save(article);
+        return responseGenerator.sendRedirect(httpRequest, "/");
     }
 }
