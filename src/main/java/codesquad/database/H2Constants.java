823f21f3a8ebc0dfd6a7a08fefa5106bfcd43aeb
jcw1031 <jcw001031@gmail.com>
2024-07-17 22:25:46 +0900

    refactor: 상수 리팩토링
    - 사용하지 않는 상수 제거
    - SQL문 상수 코드블록으로 변경

--- src/main/java/codesquad/database/H2Constants.java
+++ src/main/java/codesquad/database/H2Constants.java
@@ -2,18 +2,19 @@ package codesquad.database;
 
 public abstract class H2Constants {
 
-    public static final String JDBC_URL = "jdbc:h2:~/codestagram;DB_CLOSE_ON_EXIT=FALSE";
-    public static final String USERNAME = "sa";
-    public static final String PASSWORD = "sa";
-
-    public static final String CREATE_USER_TABLE = "CREATE TABLE IF NOT EXISTS `user` ("
-            + "id BIGINT PRIMARY KEY AUTO_INCREMENT, "
-            + "userId VARCHAR(20) NOT NULL, "
-            + "nickname VARCHAR(20) NOT NULL, "
-            + "password VARCHAR(20) NOT NULL);";
-    public static final String CREATE_ARTICLE_TABLE = "CREATE TABLE IF NOT EXISTS `article` ("
-            + "id BIGINT PRIMARY KEY AUTO_INCREMENT, "
-            + "content TEXT NOT NULL, "
-            + "userId BIGINT NOT NULL, "
-            + "FOREIGN KEY (userId) REFERENCES `user`(id));";
+    public static final String CREATE_USER_TABLE = """
+            CREATE TABLE IF NOT EXISTS `user` (
+            id BIGINT PRIMARY KEY AUTO_INCREMENT,
+            userId VARCHAR(20) NOT NULL,
+            nickname VARCHAR(20) NOT NULL,
+            password VARCHAR(20) NOT NULL
+            );
+            """;
+    public static final String CREATE_ARTICLE_TABLE = """
+            CREATE TABLE IF NOT EXISTS `article` (
+            id BIGINT PRIMARY KEY AUTO_INCREMENT,
+            content TEXT NOT NULL,
+            userId BIGINT NOT NULL
+            );
+            """;
 }
